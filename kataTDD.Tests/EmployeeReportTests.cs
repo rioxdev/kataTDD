fc6f5f2f8644b5f81dcbb33a1d9e2bb117cea957
Henrio Randriasoa <henrio.randriasoa@viseo.com>
2023-01-26 17:00:26 +0100

    Added EmployeeReport kata - stage 1

---
+++ kataTDD.Tests/EmployeeReportTests.cs
@@ -0,0 +1,43 @@
+﻿using FluentAssertions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace kataTDD.Tests;
+
+public class EmployeeReportTests
+{
+    private List<Employee> _employees;
+
+    public EmployeeReportTests()
+    {
+        _employees = new List<Employee>()
+        {
+            new Employee()
+            {
+                Name = "Empl 1", Age = 21
+            },
+
+            new Employee()
+            {
+                Name = "Empl 2", Age = 18
+            },
+
+            new Employee()
+            {
+                Name = "Empl 3", Age = 46
+            }
+        };
+    }
+
+
+    [Fact]
+    public void RepportShouldReturn18Ages()
+    {
+        var result = EmployeeReport.GetEmployees();
+
+        result.Should().Equal(_employees, (e1, e2) => e1.Age == e2.Age && e1.Name == e2.Name );
+    }
+}
---
+++ kataTDD/EmployeeReport.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace kataTDD
+{
+    public class EmployeeReport
+    {
+        private static IEnumerable<Employee> Employees
+        {
+            get
+            {
+                yield return new Employee() { Name = "Empl 1", Age = 21 };
+                yield return new Employee() { Name = "Empl 2", Age = 18 };
+                yield return new Employee() { Name = "Empl 3", Age = 46 };
+                yield return new Employee() { Name = "Empl 4", Age = 14 };
+                yield return new Employee() { Name = "Empl 5", Age = 17 };
+            }
+        }
+
+        public static List<Employee> GetEmployees()
+        {
+            return Employees.Where(e => e.Age >= 18).ToList();
+        }
+    }
+}
