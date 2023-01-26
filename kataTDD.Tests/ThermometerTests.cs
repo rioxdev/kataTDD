3c99fd8ae9b94e0303ff8e6d1ad5e8eac9a2f83a
Henrio Randriasoa <henrio.randriasoa@viseo.com>
2023-01-26 10:54:22 +0100

    Added Thermometer kata

---
+++ kataTDD.Tests/ThermometerTests.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FluentAssertions;
+
+namespace kataTDD.Tests
+{
+    public class ThermometerTests
+    {
+
+        public static IEnumerable<object[]> CreateParameters()
+        {
+            yield return new object[] { Array.Empty<int>(), 0 };
+            yield return new object[] { new int[] { 2, 9, 1 }, 1 };
+            yield return new object[] { new int[] { 2, 9, -1 }, -1 };
+            yield return new object[] { new int[] { 2, 9, -2, -3 }, 2 };
+            yield return new object[] { new int[] { -11 }, 11 };
+        }
+
+        [Theory]
+        [MemberData(nameof(CreateParameters))]
+        public void ThermometerShouldReturnsTempCloseToZero(int[] temperatures, int expected)
+        {
+            var temperature = Thermometer.GetCloseToZero(temperatures);
+            temperature.Should().Be(expected);
+        }
+
+    }
+}
---
+++ kataTDD/Thermometer.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace kataTDD
+{
+    public class Thermometer
+    {
+        public static int GetCloseToZero(int[] temperatures)
+        {
+            if (temperatures == null || !temperatures.Any())
+                return 0;
+
+            var positives = temperatures.Where(t => t >= 0);
+            var negatives = temperatures.Where(t => t < 0);
+
+            if (positives.Any() && negatives.Any())
+            {
+                var posMin = positives.Min();
+                var negMax = negatives.Max();
+
+                if (Math.Abs(negMax) < posMin)
+                    return negMax;
+                else
+                    return posMin;
+            }
+            else if (!positives.Any() ) {
+                return Math.Abs(negatives.Max());            
+            }
+            else 
+            {
+                return positives.Min();
+            }
+
+        }
+    }
+}
